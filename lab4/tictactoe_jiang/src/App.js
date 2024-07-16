2391a627f40e97fb6c5b2cd2cdf643726b8492e9
justenJiang <justenkaiz@gmail.com>
2024-07-16 00:36:07 -0400

    half done with lab4

---
+++ lab4/tictactoe_jiang/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='title'>Tic Tac Toe by 'Justen Jiang'</h1>
+      <div className='boardcontainer'>
+      <div className="board-row">
+          <Square value = "1"/>
+          <Square value = "2"/>
+          <Square value = "3"/>
+        </div>
+        <div className="board-row">
+          <Square value = "4"/>
+          <Square value = "5"/>
+          <Square value = "6"/>
+        </div>
+        <div className="board-row">
+          <Square value = "7"/>
+          <Square value = "8"/>
+          <Square value = "9"/>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+function Square({value}){
+  // click function
+  function handleClick(){
+    console.log(`clicked value ${value}!`)
+  }
+  return(
+    <button className = "square" onClick = {handleClick}>{value}</button>
+  );
+}
+
+
+export default App;
