9bf35ecaaa101e5405c081b8b4743e0f222dbaa0
justenJiang <justenkaiz@gmail.com>
2024-07-18 20:37:15 -0400

    lab6

---
+++ lab6_reactrouting/src/components/navbars.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+const Navbars = function(){
+    return(
+        <>
+            <nav className='navbar'>
+                <div><Link>QCC</Link></div>
+                <div class = "btngroup">
+                    <button><Link to = "/">Home</Link></button>
+                    <button><Link to = "/about">About us</Link></button>
+                    <button><Link to = "/contact">Contact us</Link></button>
+                </div>
+            </nav>
+        </>
+    )
+
+}
+
+export default Navbars
